1533389589b68d7ef10c569ca84452c039465a51
SurmaAa <marcinsurma98@gmail.com>
2020-08-23 12:22:57 +0200

    basic skeleton

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2020: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:node/recommended",
+    "plugin:prettier/recommended",
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    "no-var": "error",
+  },
+};
